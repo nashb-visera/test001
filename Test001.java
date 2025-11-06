8a957cbe63c47ee29a6134f2a0fefc16cb2b346a
nashb-visera <yhtseng700816@gmail.com>
2025-11-06 13:57:40 +0800

    Create Test001.java
    Initial

---
+++ Test001.java
@@ -0,0 +1,17 @@
+public class Test001
+{
+    public static void main(String[] args)
+    {
+        String str = "Java charAt() method example";
+
+        // picking the A character of the string str at index 9 using charAt() method
+        System.out.println(str.charAt(0));
+
+        // picking the ( character of the string str at index 11 using charAt() method
+        System.out.println(str.charAt(11));
+
+        // picking the ) character of the string str at index 12 using charAt() method
+        System.out.println(str.charAt(12));
+
+    }
+}
